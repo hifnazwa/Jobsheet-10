eb8da3eabddda8aaee9ef392af1cbb6b3510a16b
hifnazwa <144086644+hifnazwa@users.noreply.github.com>
2023-10-31 12:57:04 +0700

    Add files via upload

---
+++ Bioskop14_6.java
@@ -0,0 +1,27 @@
+public class Bioskop14_6 {
+    public class Bioskop14_5 {
+        public static void main(String[] args) {
+            String [][] penonton = new String [4][2];
+    
+            penonton [0][0] = "Amin";
+            penonton [0][1] = "Bena";
+            penonton [1][0] = "Candra";
+            penonton [1][1] = "Dela";
+            penonton [2][0] = "Eka";
+            penonton [2][1] = "Farhan";
+            penonton [3][0] = "Gisel";
+    
+            System.out.printf("%s \t %s\n",penonton [0][0],penonton[0][1]);
+            System.out.printf("%s \t %s\n",penonton [1][0],penonton [1][1]);
+            System.out.printf("%s \t %s\n",penonton [2][0],penonton [2][1]);
+            System.out.printf("%s \t %s\n",penonton [3][0],penonton [3][1]);
+    
+            System.out.println("Penonton pada baris ke-3: ");
+    
+            for (int i=0; i<penonton.length; i++) {
+                System.out.println("Penonton pada baris ke-"+(i+1)+": " + String.join(", ", penonton[i]));
+            }
+        }
+    }
+    
+}
